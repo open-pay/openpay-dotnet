f5dc150e15724bc4596e5dda0770a67f1e01090b
Marcos Coronado <tonik_87@hotmail.com>
2014-12-01 17:48:48 -0600

    *Se agrega test para verificar webhook y para listar los webhooks

--- OpenpayTest/WebhooksTest.cs
+++ OpenpayTest/WebhooksTest.cs
@@ -10,7 +10,7 @@ namespace OpenpayTest
 	public class WebhooksTest
 	{
 		[TestMethod]
-		public void TestCreateGetDelete()
+		public void TestWebhooks_Create_Get_Verify_Get_List_Delete()
 		{
 			OpenpayAPI openpayAPI = new OpenpayAPI(Constants.API_KEY, Constants.MERCHANT_ID);
 			Webhook webhook = new Webhook();
@@ -29,7 +29,69 @@ namespace OpenpayTest
 			Assert.AreEqual("unverified", webhookGet.Status);
 			Assert.AreEqual(2, webhookGet.EventTypes.Count);
 
+			openpayAPI.WebhooksService.Verify(webhookGet.Id, webhookGet.Url);
+
+			webhookGet = openpayAPI.WebhooksService.Get(webhookCreated.Id);
+			Assert.IsNotNull(webhookGet.Id);
+			Assert.IsNotNull(webhookGet.Status);
+			Assert.AreEqual("verified", webhookGet.Status);
+			Assert.AreEqual(2, webhookGet.EventTypes.Count);
+
+			List<webhook> webhooksList = openpayAPI.WebhooksService.List(webhookCreated.Id);
+			Assert.AreEqual(1, webhooksList.Count);
+
 			openpayAPI.WebhooksService.Delete(webhookGet.Id);
+		}
+
+		private string GetVerificationCode(string url_webhook)
+		{
+			string url = url_webhook + "?inspect";
+
+			WebRequest req = (WebRequest)WebRequest.Create(url);
+			req.Method = "POST";
+			if (req is HttpWebRequest)
+			{
+				((HttpWebRequest)req).UserAgent = user_agent;
+			}
+			//req.Credentials = credential;
+			req.PreAuthenticate = false;
+			req.Timeout = 60 * 1000;
+			req.ContentType = "application/json";
+
+			// Obteniendo respuesta
+			string result = null;
+
+			try
+			{
+				using (WebResponse resp = (WebResponse)req.GetResponse())
+				{
+					result = GetResponseAsString(resp);
+				}
+			}
+			catch (WebException wexc)
+			{
+				if (wexc.Response != null)
+				{
+					string json_error = GetResponseAsString(wexc.Response);
+					HttpStatusCode status_code = HttpStatusCode.BadRequest;
+					HttpWebResponse resp = wexc.Response as HttpWebResponse;
+					if (resp != null)
+						status_code = resp.StatusCode;
+
+					if ((int)status_code <= 500)
+						throw OpenpayException.GetFromJSON(status_code, json_error);
+				}
+				throw;
+			}
+			return result.Substring (result.IndexOf ("verification_code") + 28, 8);
+		}
+
+		private string GetResponseAsString(WebResponse response)
+		{
+			using (StreamReader sr = new StreamReader(response.GetResponseStream(), encoding))
+			{
+				return sr.ReadToEnd();
+			}
 		}
 	}
 }
