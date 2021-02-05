5fb07f628ef6b26c2450afd4e2eecbff6670e425
Marco Morales <marco.morales@tcpip.tech>
2021-02-04 19:08:34 -0600

    Se agrega clase url util con metodo para escapear "[" y "]" de url

---
+++ Openpay/Utils/UrlUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+
+namespace Openpay.Utils
+{
+    public class UrlUtils
+    {
+        public string ScapeSquareBrackets(string url)
+        {
+            url = url.Replace("[", System.Net.WebUtility.UrlEncode("["));
+            url = url.Replace("]", System.Net.WebUtility.UrlEncode("]"));
+            return url;
+        }
+    }
+}
